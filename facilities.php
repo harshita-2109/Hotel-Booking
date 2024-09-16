b84f2b216abaac9f204514016706cd3e4d119981
tanisha-0230 <tanisha4921@gmail.com>
2024-09-16 21:53:44 +0530

    update: facilities

--- facilities.php
+++ facilities.php
@@ -31,90 +31,28 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/wifi.svg" width="40px">
-                    <h5 class="m-0 ms-3">WiFi</h5>
-                    </div>
-                    
-                    
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odio 
-                        molestiae libero perferendis veritatis enim voluptatibus iusto nam.</p>
-                </div>
-            </div>
+            <?php
+                $res = selectAll('facilities');
+                $path = FACILITIES_IMG_PATH;
 
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/ac.svg" width="40px">
-                    <h5 class="m-0 ms-3">AC</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odio 
-                        molestiae libero perferendis veritatis enim voluptatibus iusto nam.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/tv.svg" width="40px">
-                    <h5 class="m-0 ms-3">TV</h5>
-                    </div>
-                    
-                    
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odio 
-                        molestiae libero perferendis veritatis enim voluptatibus iusto nam.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/heater.svg" width="40px">
-                    <h5 class="m-0 ms-3">Gyeser</h5>
-                    </div>
-                    
-                    
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odio 
-                        molestiae libero perferendis veritatis enim voluptatibus iusto nam.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/xyz.svg" width="40px">
-                    <h5 class="m-0 ms-3">Room Heater</h5>
-                    </div>
-                    
-                    
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odio 
-                        molestiae libero perferendis veritatis enim voluptatibus iusto nam.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                    <img src="images/facilities/abc.svg" width="40px">
-                    <h5 class="m-0 ms-3">Wellness Treatment</h5>
-                    </div>
-                    
-                    
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odio 
-                        molestiae libero perferendis veritatis enim voluptatibus iusto nam.</p>
-                </div>
-            </div>
-
+                while($row = mysqli_fetch_assoc(($res))){
+                    echo<<<data
+                        <div class="col-lg-4 col-md-6 mb-5 px-4">
+                            <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                                <div class="d-flex align-items-center mb-2">
+                                    <img src="$path$row[icon]" width="40px">
+                                    <h5 class="m-0 ms-3">$row[name]</h5>
+                                </div>
+                                <p>$row[description]</p>
+                            </div>
+                        </div>
+                    data;
+                }
+            ?>
         </div>
     </div>
 
-
-
-
-    <?php require('inc/footer.php');
-    ?>
+    <?php require('inc/footer.php'); ?>
 
 </body>
 </html>
